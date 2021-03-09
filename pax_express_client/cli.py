1c0657046cda560866833c3e2ab22a7bf4f70703
Florian Ludwig <f.ludwig@greyrook.com>
2021-03-09 09:53:55 +0100

    add `self version` command

--- pax_express_client/cli.py
+++ pax_express_client/cli.py
@@ -1,3 +1,4 @@
+import pax_express_client
 from typer import Typer
 from pax_express_client import (
     repo_cli,
@@ -14,6 +15,16 @@ cli.add_typer(authentication_cli, name="auth", help="login and logout")
 cli.add_typer(package_cli, name="package", help="Work with packages")
 cli.add_typer(version_cli, name="version", help="Work with versions")
 cli.add_typer(file_cli, name="file", help="Work with files")
+
+
+self_cli = Typer(name="self")
+@self_cli.command(help="Display cli tool version")
+def version():
+    print(pax_express_client.__version__)
+
+
+cli.add_typer(self_cli, name="self")
+
 
 if __name__ == "__main__":
     cli()
