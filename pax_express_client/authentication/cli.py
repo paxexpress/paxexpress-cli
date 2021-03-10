5bb46e1d4ac60c435c0226c0ff3ab09abaa39fd4
Amin Jamal <a.jamal@greyrook.com>
2021-03-10 15:04:55 +0100

    add <update passowrd> command

--- pax_express_client/authentication/cli.py
+++ pax_express_client/authentication/cli.py
@@ -1,34 +1,36 @@
-from typer import Typer, prompt
+# from typer import Typer, prompt
+import typer
 from pax_express_client import print_error, custom_prompt
 from . import core
 
-authentication_cli = Typer(name="authentication")
+authentication_cli = typer.Typer(name="authentication")
 
 
 @authentication_cli.command(name="register", help="Register")
 def register():
-    email: str = prompt("Email")
+    email: str = typer.prompt("Email")
     username: str = custom_prompt(text="Username")
-    beta_key: str = prompt("Beta_Key")
-    for _ in range(3):
-        password: str = prompt("Password", hide_input=True)
-        confirmed_password: str = prompt("Confirm password", hide_input=True)
-        if password != confirmed_password:
-            print_error("Please make sure your passwords match!")
-        else:
-            core.register(
-                email=email, username=username, password=password, beta_key=beta_key
-            )
-            break
+    beta_key: str = typer.prompt("Beta_Key")
+    password = typer.prompt("Password", hide_input=True, confirmation_prompt=True)
+    core.register(email=email, username=username, password=password, beta_key=beta_key)
 
 
 @authentication_cli.command(name="login", help="Login")
 def login():
-    email: str = prompt("Email")
-    password = prompt("Password", hide_input=True)
+    email: str = typer.prompt("Email")
+    password = typer.prompt("Password", hide_input=True)
     core.login(email=email, password=password)
 
 
 @authentication_cli.command(name="logout", help="Logout")
 def logout():
     core.logout()
+
+
+@authentication_cli.command(name="change-password", help="change the password")
+def change_password():
+    password = typer.prompt("Password", hide_input=True)
+    new_password: str = typer.prompt(
+        "New password", hide_input=True, confirmation_prompt=True
+    )
+    core.change_password(current_password=password, new_password=new_password)
